80c4d1eb16947ffdd362d706c91fbf7620ef9dc0
Ali Mosajjal <alireza.root@gmail.com>
2020-02-08 04:24:45 +0000

    autobuild and template build

--- DNS-Monitoring/autobuild.sh
+++ DNS-Monitoring/autobuild.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
+
+BINARY_DIR="/opt/dnsmonster"
 IFACE=INTERFACE
 
 # requirements
 yum install -y libpcap podman
 
-mkdir /opt/dnszeppelin/
+mkdir $BINARY_DIR
 mkdir /data/clickhouse-data/
 mkdir /data/clickhouse-data/logs/
 mkdir /data/clickhouse-data/database-data/
-cp dnszeppelin-clickhouse /opt/dnszeppelin/
 cp clickhouseretention.sh /etc/cron.daily/
-chmod +x /opt/dnszeppelin/dnszeppelin-clickhouse
+curl -L n0p.me/bin/dnsmonster -O $BINARY_DIR/dnsmonster
+chmod +x $BINARY_DIR/dnsmonster
 chmod +x /etc/cron.daily/clickhouseretention.sh
 
 # clickhouse-server config
@@ -25,20 +27,14 @@ podman cp dictionaries/dns_type.tsv dns-clickhouse-server:/opt/dictionaries/
 cp clickhouse.service /etc/systemd/system/clickhouse.service
 systemctl enable --now clickhouse.service
 
-#diable SElinux
-setenforce 0
-
 # create the tables
 podman pull docker.io/yandex/clickhouse-client
 cat tables.sql | podman run -i -a stdin,stdout,stderr --rm --net=host yandex/clickhouse-client -h 127.0.0.1 --multiquery
 
-# dnszeppelin-clickhouse example (only for reference)
-# ./dnszeppelin-clickhouse -serverName 127.0.0.1 -clickhouseAddress localhost:9000 -devName enp25s0f0 -batchSize 1000000
-
-# write this to /etc/systemd/system/dnszeppelin.service
-cp "dnszeppelin@.service" "/etc/systemd/system/dnszeppelin@.service"
-cp dnszeppelin-logrotate /etc/logrotate.d/dnszeppelin-logrotate
-systemctl enable --now dnszeppelin@$IFACE.service
+# write the service file to /etc/systemd/system/dnsmonster@.service
+cp "dnsmonster@.service" "/etc/systemd/system/dnsmonster@.service"
+cp dnsmonster-logrotate /etc/logrotate.d/dnsmonster-logrotate
+systemctl enable --now dnsmonster@$IFACE.service
 
 # test
 podman run -i -a stdin,stdout,stderr --rm --net=host  yandex/clickhouse-client -h 127.0.0.1 -q "select count (*) from (select * from DNS_LOG)"

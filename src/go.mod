24155bb13c60098c43fb79255ac30ac3215565f4
Alireza Mosajjal <mosajjal@gmail.com>
2020-12-29 12:57:55 +1300

    added config file and env variable support

--- src/go.mod
+++ src/go.mod
@@ -8,9 +8,8 @@ require (
 	github.com/ClickHouse/clickhouse-go v1.4.3
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/miekg/dns v1.1.35
+	github.com/namsral/flag v1.7.4-pre
 	github.com/rogpeppe/fastuuid v1.2.0
-	github.com/satori/go.uuid v1.2.0
 	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b
 	golang.org/x/sys v0.0.0-20201106081118-db71ae66460a // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- src/main.go
+++ src/main.go
@@ -1,44 +1,51 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"os"
 	"runtime"
 	"runtime/pprof"
 	"sync"
 	"time"
+
+	"github.com/namsral/flag"
 )
 
-var devName = flag.String("devName", "", "Device used to capture")
-var pcapFile = flag.String("pcapFile", "", "Pcap filename to run")
+var fs = flag.NewFlagSetWithEnvPrefix(os.Args[0], "DNSMONSTER", 0)
+var devName = fs.String("devName", "", "Device used to capture")
+var pcapFile = fs.String("pcapFile", "", "Pcap filename to run")
 
 // Filter is not using "(port 53)", as it will filter out fragmented udp packets, instead, we filter by the ip protocol
 // and check again in the application.
-var filter = flag.String("filter", "((ip and (ip[9] == 6 or ip[9] == 17)) or (ip6 and (ip6[6] == 17 or ip6[6] == 6 or ip6[6] == 44)))", "BPF filter applied to the packet stream. If port is selected, the packets will not be defragged.")
-var port = flag.Uint("port", 53, "Port selected to filter packets")
-var gcTime = flag.Uint("gcTime", 10, "Time in seconds to garbage collect the tcp assembly and ip defragmentation")
-var clickhouseAddress = flag.String("clickhouseAddress", "localhost:9000", "Address of the clickhouse database to save the results")
-var clickhouseDelay = flag.Uint("clickhouseDelay", 1, "Number of seconds to batch the packets")
-var maskSize = flag.Int("maskSize", 32, "Mask source IPs by bits. 32 means all the bits of IP is saved in DB")
-var serverName = flag.String("serverName", "default", "Name of the server used to index the metrics.")
-var batchSize = flag.Uint("batchSize", 100000, "Minimun capacity of the cache array used to send data to clickhouse. Set close to the queries per second received to prevent allocations")
-var packetHandlerCount = flag.Uint("packetHandlers", 1, "Number of routines used to handle received packets")
-var tcpHandlerCount = flag.Uint("tcpHandlers", 1, "Number of routines used to handle tcp assembly")
-var useAfpacket = flag.Bool("useAfpacket", false, "Use AFPacket for live captures")
-var packetChannelSize = flag.Uint("packetHandlerChannelSize", 100000, "Size of the packet handler channel")
-var tcpAssemblyChannelSize = flag.Uint("tcpAssemblyChannelSize", 1000, "Size of the tcp assembler")
-var tcpResultChannelSize = flag.Uint("tcpResultChannelSize", 1000, "Size of the tcp result channel")
-var resultChannelSize = flag.Uint("resultChannelSize", 100000, "Size of the result processor channel size")
-var defraggerChannelSize = flag.Uint("defraggerChannelSize", 500, "Size of the channel to send packets to be defragged")
-var defraggerChannelReturnSize = flag.Uint("defraggerChannelReturnSize", 500, "Size of the channel where the defragged packets are returned")
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
-var memprofile = flag.String("memprofile", "", "write memory profile to file")
-var loggerFilename = flag.Bool("loggerFilename", false, "Show the file name and number of the logged string")
-var packetLimit = flag.Int("packetLimit", 0, "Limit of packets logged to clickhouse every iteration. Default 0 (disabled)")
+var config = fs.String(flag.DefaultConfigFlagname, "", "path to config file")
+var filter = fs.String("filter", "((ip and (ip[9] == 6 or ip[9] == 17)) or (ip6 and (ip6[6] == 17 or ip6[6] == 6 or ip6[6] == 44)))", "BPF filter applied to the packet stream. If port is selected, the packets will not be defragged.")
+var port = fs.Uint("port", 53, "Port selected to filter packets")
+var gcTime = fs.Uint("gcTime", 10, "Time in seconds to garbage collect the tcp assembly and ip defragmentation")
+var clickhouseAddress = fs.String("clickhouseAddress", "localhost:9000", "Address of the clickhouse database to save the results")
+var clickhouseDelay = fs.Uint("clickhouseDelay", 1, "Number of seconds to batch the packets")
+var captureStatsDelay = fs.Duration("captureStatsDelay", time.Second, "Number of seconds to calculate interface stats")
+var printStatsDelay = fs.Duration("printStatsDelay", time.Second*10, "Number of seconds to print capture and database stats")
+var maskSize = fs.Int("maskSize", 32, "Mask source IPs by bits. 32 means all the bits of IP is saved in DB")
+var serverName = fs.String("serverName", "default", "Name of the server used to index the metrics.")
+var batchSize = fs.Uint("batchSize", 100000, "Minimun capacity of the cache array used to send data to clickhouse. Set close to the queries per second received to prevent allocations")
+var packetHandlerCount = fs.Uint("packetHandlers", 1, "Number of routines used to handle received packets")
+var tcpHandlerCount = fs.Uint("tcpHandlers", 1, "Number of routines used to handle tcp assembly")
+var useAfpacket = fs.Bool("useAfpacket", false, "Use AFPacket for live captures")
+var afPacketBuffersizeMb = fs.Uint("AfpacketBuffersizeMb", 64, "Afpacket Buffersize in MB")
+var packetChannelSize = fs.Uint("packetHandlerChannelSize", 100000, "Size of the packet handler channel")
+var tcpAssemblyChannelSize = fs.Uint("tcpAssemblyChannelSize", 1000, "Size of the tcp assembler")
+var tcpResultChannelSize = fs.Uint("tcpResultChannelSize", 1000, "Size of the tcp result channel")
+var resultChannelSize = fs.Uint("resultChannelSize", 100000, "Size of the result processor channel size")
+var defraggerChannelSize = fs.Uint("defraggerChannelSize", 500, "Size of the channel to send packets to be defragged")
+var defraggerChannelReturnSize = fs.Uint("defraggerChannelReturnSize", 500, "Size of the channel where the defragged packets are returned")
+var cpuprofile = fs.String("cpuprofile", "", "write cpu profile to file")
+var memprofile = fs.String("memprofile", "", "write memory profile to file")
+var loggerFilename = fs.Bool("loggerFilename", false, "Show the file name and number of the logged string")
+var packetLimit = fs.Int("packetLimit", 0, "Limit of packets logged to clickhouse every iteration. Default 0 (disabled)")
 
 func checkFlags() {
-	flag.Parse()
+
+	fs.Parse(os.Args[1:])
 	if *loggerFilename {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 	} else {

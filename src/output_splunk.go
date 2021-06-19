a2414b526b9140a4f2bbb2b1119e0314d99c92e1
Alireza Mosajjal <mosajjal@gmail.com>
2021-06-19 13:51:51 +1200

    splunk now uses the new typed config

--- src/output_splunk.go
+++ src/output_splunk.go
@@ -7,7 +7,6 @@ import (
 	"math/rand"
 	"net/http"
 	"strings"
-	"sync"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -17,29 +16,29 @@ import (
 
 var splunkStats = outputStats{"splunk", 0, 0}
 
-func connectMultiSplunkRetry(exiting chan bool, splunkEndpoints []string, splunkHecToken string, skipTlsVerification bool) []*splunk.Client {
+func connectMultiSplunkRetry(spConfig splunkConfig) []*splunk.Client {
 	var outputs []*splunk.Client
-	for _, splunkEndpoint := range splunkEndpoints {
-		outputs = append(outputs, connectSplunkRetry(exiting, splunkEndpoint, splunkHecToken, skipTlsVerification))
+	for _, splunkEndpoint := range spConfig.splunkOutputEndpoints {
+		outputs = append(outputs, connectSplunkRetry(spConfig, splunkEndpoint))
 	}
 	return outputs
 }
 
-func connectSplunkRetry(exiting chan bool, splunkEndpoint string, splunkHecToken string, skipTlsVerification bool) *splunk.Client {
+func connectSplunkRetry(spConfig splunkConfig, splunkEndpoint string) *splunk.Client {
 	tick := time.NewTicker(5 * time.Second)
 	// don't retry connection if we're doing dry run
-	if *splunkOutputType == 0 {
+	if spConfig.splunkOutputType == 0 {
 		tick.Stop()
 	}
 	defer tick.Stop()
 	for {
-		conn, err := connectSplunk(exiting, splunkEndpoint, splunkHecToken, skipTlsVerification)
+		conn, err := connectSplunk(spConfig, splunkEndpoint)
 		if err == nil {
 			return conn
 		}
 		// Error getting connection, wait the timer or check if we are exiting
 		select {
-		case <-exiting:
+		case <-spConfig.general.exiting:
 			// When exiting, return immediately
 			return nil
 		case <-tick.C:
@@ -48,9 +47,9 @@ func connectSplunkRetry(exiting chan bool, splunkEndpoint string, splunkHecToken
 	}
 }
 
-func connectSplunk(exiting chan bool, splunkEndpoint string, splunkHecToken string, skipTlsVerification bool) (*splunk.Client, error) {
+func connectSplunk(spConfig splunkConfig, splunkEndpoint string) (*splunk.Client, error) {
 
-	tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: skipTlsVerification}}
+	tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: spConfig.general.skipTlsVerification}}
 	httpClient := &http.Client{Timeout: time.Second * 20, Transport: tr}
 
 	splunkURL := splunkEndpoint
@@ -61,7 +60,7 @@ func connectSplunk(exiting chan bool, splunkEndpoint string, splunkHecToken stri
 	client := splunk.NewClient(
 		httpClient,
 		splunkURL,
-		splunkHecToken,
+		spConfig.splunkOutputToken,
 		"",
 		"",
 		"",
@@ -70,32 +69,32 @@ func connectSplunk(exiting chan bool, splunkEndpoint string, splunkHecToken stri
 	return client, err
 }
 
-func splunkOutput(resultChannel chan DNSResult, exiting chan bool, wg *sync.WaitGroup, splunkEndpoints []string, splunkHecToken string, splunkIndex string, splunkBatchSize uint, batchDelay time.Duration, limit int) {
-	wg.Add(1)
-	defer wg.Done()
+func splunkOutput(spConfig splunkConfig) {
+	spConfig.general.wg.Add(1)
+	defer spConfig.general.wg.Done()
 
-	clients := connectMultiSplunkRetry(exiting, splunkEndpoints, splunkHecToken, *skipTlsVerification)
+	clients := connectMultiSplunkRetry(spConfig)
 
-	batch := make([]DNSResult, 0, splunkBatchSize)
+	batch := make([]DNSResult, 0, spConfig.splunkBatchSize)
 	rand.Seed(time.Now().Unix())
-	ticker := time.Tick(batchDelay)
-	printStatsTicker := time.Tick(*printStatsDelay)
+	ticker := time.Tick(spConfig.splunkBatchDelay)
+	printStatsTicker := time.Tick(spConfig.general.printStatsDelay)
 
 	for {
 		select {
 		case data := <-resultChannel:
-			if limit == 0 || len(batch) < limit {
+			if spConfig.general.packetLimit == 0 || len(batch) < spConfig.general.packetLimit {
 				batch = append(batch, data)
 			}
 		case <-ticker:
 			client := clients[rand.Intn(len(clients))]
-			if err := splunkSendData(client, splunkIndex, *splunkOutputSource, *splunkOutputSourceType, batch); err != nil {
+			if err := splunkSendData(client, batch, spConfig); err != nil {
 				log.Info(err)
-				client = connectSplunkRetry(exiting, client.URL, splunkHecToken, *skipTlsVerification)
+				client = connectSplunkRetry(spConfig, client.URL)
 			} else {
-				batch = make([]DNSResult, 0, splunkBatchSize)
+				batch = make([]DNSResult, 0, spConfig.splunkBatchSize)
 			}
-		case <-exiting:
+		case <-spConfig.general.exiting:
 			return
 		case <-printStatsTicker:
 			log.Infof("output: %+v", splunkStats)
@@ -103,11 +102,11 @@ func splunkOutput(resultChannel chan DNSResult, exiting chan bool, wg *sync.Wait
 	}
 }
 
-func splunkSendData(client *splunk.Client, splunkIndex string, splunkSource string, splunkSourceType string, batch []DNSResult) error {
+func splunkSendData(client *splunk.Client, batch []DNSResult, spConfig splunkConfig) error {
 	var events []*splunk.Event
 	for i := range batch {
 		for _, dnsQuery := range batch[i].DNS.Question {
-			if checkIfWeSkip(*splunkOutputType, dnsQuery.Name) {
+			if checkIfWeSkip(spConfig.splunkOutputType, dnsQuery.Name) {
 				splunkStats.Skipped++
 				continue
 			}
@@ -116,7 +115,7 @@ func splunkSendData(client *splunk.Client, splunkIndex string, splunkSource stri
 			errorHandler(err)
 			events = append(
 				events,
-				client.NewEventWithTime(batch[i].Timestamp, string(fullQuery), splunkSource, splunkSourceType, splunkIndex),
+				client.NewEventWithTime(batch[i].Timestamp, string(fullQuery), spConfig.splunkOutputSource, spConfig.splunkOutputSourceType, spConfig.splunkOutputIndex),
 			)
 		}
 	}

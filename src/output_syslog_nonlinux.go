460ecc0116ab7ac0e43d7d8158e22e72ce6d3b4a
Alireza Mosajjal <mosajjal@gmail.com>
2021-08-10 13:34:36 +1200

    syslog windows and non-windows quick fiX

---
+++ src/output_syslog_nonlinux.go
@@ -0,0 +1,21 @@
+// +build !linux
+
+// This entire file is a dummy one to make sure all our cross platform builds work even if the underlying OS doesn't suppot some of the functionality
+// afpacket is a Linux-only feature, so we want the relevant function to technically "translate" to something here, which basically returns an error
+
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+)
+
+var syslog struct {
+	Writer      bool
+	Dial        bool
+	LOG_WARNING bool
+	LOG_DAEMON  bool
+}
+
+func syslogOutput(sysConfig syslogConfig) {
+	log.Error("No Syslog is supported in Windows")
+}

8c287fec787bfbb2bc8acbb71394c3ec6ce89bba
Alireza Mosajjal <mosajjal@gmail.com>
2021-09-05 11:58:39 +1200

    changes to Grafana panel and SQL tables

--- clickhouse/tables.sql
+++ clickhouse/tables.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS DNS_LOG (
   timestamp DateTime,
   Server String,
   IPVersion UInt8,
-  IPPrefix UInt32,
+  SrcIP UInt64,
+  DstIP UInt64,
   Protocol FixedString(3),
   QR UInt8,
   OpCode UInt8,
@@ -66,6 +67,16 @@ ENGINE=AggregatingMergeTree(DnsDate, (timestamp, Server), 8192) AS
 
 
 -- View wih query OpCode
+CREATE MATERIALIZED VIEW IF NOT EXISELECT DnsDate, timestamp, Server, sumState(Size) AS TotalSize, avgState(Size) AS AverageSize FROM DNS_LOG GROUP BY Server, DnsDate, timestamp;
+
+
+-- View with edns information
+CREATE MATERIALIZED VIEW IF NOT EXISTS DNS_EDNS
+ENGINE=AggregatingMergeTree(DnsDate, (timestamp, Server), 8192) AS
+  SELECT DnsDate, timestamp, Server, sumState(Edns0Present) as EdnsCount, sumState(DoBit) as DoBitCount FROM DNS_LOG WHERE QR=0 GROUP BY Server, DnsDate, timestamp;
+
+
+-- View wih query OpCode
 CREATE MATERIALIZED VIEW IF NOT EXISTS DNS_OPCODE
 ENGINE=SummingMergeTree
   PARTITION BY toYYYYMMDD(DnsDate)
@@ -111,13 +122,70 @@ ENGINE=SummingMergeTree
   AS SELECT DnsDate, timestamp, Server, ResponseCode, count(*) as c FROM DNS_LOG WHERE QR=1 GROUP BY Server, DnsDate, timestamp, ResponseCode;    
 
 
--- View with IP Prefix
-CREATE MATERIALIZED VIEW IF NOT EXISTS DNS_IP_MASK
+-- View with Source IP Prefix
+CREATE MATERIALIZED VIEW IF NOT EXISTS DNS_SRCIP_MASK
 ENGINE=SummingMergeTree
   PARTITION BY toYYYYMMDD(DnsDate)
-  PRIMARY KEY  (timestamp, Server, IPVersion, IPPrefix)
-  ORDER BY  (timestamp, Server, IPVersion, IPPrefix)
-  SAMPLE BY IPPrefix
+  PRIMARY KEY  (timestamp, Server, OpCode)
+  ORDER BY  (timestamp, Server, OpCode)
+  SAMPLE BY OpCode
   TTL DnsDate + INTERVAL 30 DAY -- DNS_TTL_VARIABLE
   SETTINGS index_granularity = 8192
-  AS SELECT DnsDate, timestamp, Server, IPVersion, IPPrefix, count(*) as c FROM DNS_LOG GROUP BY Server, DnsDate, timestamp, IPVersion, IPPrefix ;  
+  AS SELECT DnsDate, timestamp, Server, OpCode, count(*) as c FROM DNS_LOG WHERE QR=0 GROUP BY Server, DnsDate, timestamp, OpCode;
+
+
+-- View with Query Types
+CREATE MATERIALIZED VIEW IF NOT EXISTS DNS_TYPE
+ENGINE=SummingMergeTree 
+  PARTITION BY toYYYYMMDD(DnsDate)
+  PRIMARY KEY  (timestamp, Server, Type)
+  ORDER BY  (timestamp, Server, Type)
+  SAMPLE BY Type
+  TTL DnsDate + INTERVAL 30 DAY -- DNS_TTL_VARIABLE
+  SETTINGS index_granularity = 8192
+  AS   SELECT DnsDate, timestamp, Server, Type, count(*) as c FROM DNS_LOG WHERE QR=0 GROUP BY Server, DnsDate, timestamp, Type;
+
+-- View with Query Class
+CREATE MATERIALIZED VIEW IF NOT EXISTS DNS_CLASS
+ENGINE=SummingMergeTree
+  PARTITION BY toYYYYMMDD(DnsDate)
+  PRIMARY KEY  (timestamp, Server, Class)
+  ORDER BY  (timestamp, Server, Class)
+  SAMPLE BY Class
+  TTL DnsDate + INTERVAL 30 DAY -- DNS_TTL_VARIABLE
+  SETTINGS index_granularity = 8192
+  AS SELECT DnsDate, timestamp, Server, Class, count(*) as c FROM DNS_LOG WHERE QR=0 GROUP BY Server, DnsDate, timestamp, Class;  
+
+-- View with query responses
+CREATE MATERIALIZED VIEW IF NOT EXISTS DNS_RESPONSECODE
+ENGINE=SummingMergeTree
+  PARTITION BY toYYYYMMDD(DnsDate)
+  PRIMARY KEY  (timestamp, Server, ResponseCode)
+  ORDER BY  (timestamp, Server, ResponseCode)
+  SAMPLE BY ResponseCode
+  TTL DnsDate + INTERVAL 30 DAY -- DNS_TTL_VARIABLE
+  SETTINGS index_granularity = 8192
+  AS SELECT DnsDate, timestamp, Server, ResponseCode, count(*) as c FROM DNS_LOG WHERE QR=1 GROUP BY Server, DnsDate, timestamp, ResponseCode;    
+
+
+-- View with Source IP Prefix
+CREATE MATERIALIZED VIEW IF NOT EXISTS DNS_SRCIP_MASK
+ENGINE=SummingMergeTree
+  PARTITION BY toYYYYMMDD(DnsDate)
+  PRIMARY KEY  (timestamp, Server, IPVersion, SrcIP)
+  ORDER BY  (timestamp, Server, IPVersion, SrcIP)
+  SAMPLE BY SrcIP
+  TTL DnsDate + INTERVAL 30 DAY -- DNS_TTL_VARIABLE
+  SETTINGS index_granularity = 8192
+  AS SELECT DnsDate, timestamp, Server, IPVersion, SrcIP, count(*) as c FROM DNS_LOG GROUP BY Server, DnsDate, timestamp, IPVersion, SrcIP ;  
+
+-- View with Destination IP Prefix
+CREATE MATERIALIZED VIEW IF NOT EXISTS DNS_DSTIP_MASK
+ENGINE=SummingMergeTree
+  PARTITION BY toYYYYMMDD(DnsDate)
+  PRIMARY KEY  (timestamp, Server, IPVersion, DstIP)
+  ORDER BY  (timestamp, Server, IPVersion, DstIP)
+  SAMPLE BY DstIP
+  TTL DnsDate + INTERVAL 30 DAY -- DNS_TTL_VARIABLE
+  SETTINGS index_granularity = 8192
+  AS SELECT DnsDate, timestamp, Server, IPVersion, DstIP, count(*) as c FROM DNS_LOG GROUP BY Server, DnsDate, timestamp, IPVersion, DstIP ;  

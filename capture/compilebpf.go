f363fd950da98a78d230bcbb80217ef33c18cbcb
Alireza Mosajjal <mosajjal@gmail.com>
2022-01-23 00:39:00 +1300

    code cleanup and minor bugfix

--- capture/compilebpf.go
+++ capture/compilebpf.go
@@ -15,7 +15,7 @@ func TcpdumpToPcapgoBpf(filter string) []bpf.RawInstruction {
 	returnByteCodes := []bpf.RawInstruction{}
 	bytecodes, err := pcap.CompileBPFFilter(layers.LinkTypeEthernet, 1024, filter)
 	for _, ins := range bytecodes {
-		returnByteCodes = append(returnByteCodes, bpf.RawInstruction{ins.Code, ins.Jt, ins.Jf, ins.K})
+		returnByteCodes = append(returnByteCodes, bpf.RawInstruction{Op: ins.Code, Jt: ins.Jt, Jf: ins.Jf, K: ins.K})
 	}
 	if err != nil {
 		return nil

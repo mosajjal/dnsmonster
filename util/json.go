9e4dd9f4238ee78ba0ab731f8e461fab46a3bfd2
Alireza Mosajjal <mosajjal@gmail.com>
2022-05-28 12:05:29 +1200

    use sonic json universally

--- util/json.go
+++ util/json.go
@@ -1,14 +1,13 @@
-//go:build !linux || !amd64
-// +build !linux !amd64
-
 package util
 
-import "encoding/json"
+import (
+	"github.com/bytedance/sonic"
+)
 
 type JsonOutput struct{}
 
 func (j JsonOutput) Marshal(d DNSResult) string {
-	res, _ := json.Marshal(d)
+	res, _ := sonic.Marshal(d)
 	return string(res)
 }
 
